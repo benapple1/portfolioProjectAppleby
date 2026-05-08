ecc3d1b57013d5ecdc87c26503826b3a542cfa9e
Ben Appleby <benjiappleby1@gmail.com>
2026-05-08 14:47:58 -0500

    Added Google Flow AI Video info and Embedded MP4 file

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.staticfiles import finders
 from django.views.generic import DetailView, ListView, TemplateView
 
 from .models import Project
@@ -27,6 +28,11 @@ class ProjectDetailView(DetailView):
     model = Project
     template_name = "portfolio/project_detail.html"
     context_object_name = "project"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["google_flow_video_exists"] = bool(finders.find("videos/googleFlowVideo.mp4"))
+        return context
 
 
 class SkillsView(TemplateView):
